f59285c788ca131036f6b1e45771502a99a2c42f
Lindsey <linds.fonnes@gmail.com>
2021-07-02 10:57:42 -0600

    fixed step 3

--- step3.js
+++ step3.js
@@ -1,46 +1,60 @@
 const axios = require("axios");
 const fs = require("fs");
 
-function cat(path){
+//passes in the path to be read, if there's an error then show error, otherwise handle output
+function cat(path,out){
     fs.readFile(path,"utf8", function(err,data){
         if (err){
             console.log("ERROR:",err);
             process.exit(1);
+        } else {
+            handleOutput(data,out);
         }
-        console.log(data);
     })
 }
 
-async function webCat(url){
+//get resp from axios and pass to handleoutput, catch error
+async function webCat(url,out){
     try{
         let resp = await axios.get(url);
-        console.log(resp.data);
+        handleOutput(resp.data,out)
     } catch(err){
         console.log(`Error fetching ${url}:`,err);
+        process.exit(1)
     } 
 }
 
-function handleOutput(new_text,path){
-    fs.writeFile(path,new_text,"utf8", err => {
-        if(err){
-            console.log(`Error writing to ${path}`, err)
-            process.kill(1)
-        }
-        console.log("it worked")
-    })
+//if there is an out defined, then write to new file & don't console.log anything. else just console.log what was given from webCat or cat
+function handleOutput(text,out){
+    if(out){
+        fs.writeFile(out,text,"utf8", err => {
+            if(err){
+                console.log(`Error writing to ${out}: ${err}`)
+                process.exit(1)
+            }
+        })
+    } else{
+        console.log(text)
+    }
 }
 
-let arg = process.argv[2];
+//declare path & out 
+let path;
+let out;
 
-if(arg.slice(0,4) === "http"){
-    webCat(arg);
-} else if (arg === "--out"){
-    let txt = process.argv[3]
-    let file = process.argv[4]
-    handleOutput(txt,file)
+//if --out then they want the function to run w/o output  
+if (process.argv[2] === "--out"){
+    out = process.argv[3];
+    path = process.argv[4];
+} else {
+    path = process.argv[2];
 }
-else {
-    cat(arg);
+
+//which function to run based on type of path provided
+if(path.slice(0,4) === "http"){
+    webCat(path,out);
+} else {
+    cat(path,out);
 }
 
 
